3c7a9a327c779738547a5fcdb8190e329810c4c7
MisterDoom <raulignatus@yahoo.com>
2016-10-29 20:51:36 +0100

    Stegano supports jpeg

--- steganoTool/stegano.py
+++ steganoTool/stegano.py
@@ -1,9 +1,24 @@
-from PIL import Image, ImageMath
+from PIL import Image, ImageMath, ImageColor
+import webcolors
 import sys
 
+colorNames = ['red', 'green', 'blue']
+
 def encrypt(original, watermark, output):
-    red, green, blue, alpha = original.split()
-    wred, wgreen, wblue, walpha = watermark.split()
+    switcher = False
+    if original.endswith('.jpg'):
+        switcher = True
+
+    watermark = Image.open(watermark)
+    original = Image.open(original)
+    watermark = watermark.resize(original.size)
+
+    if switcher:
+        red, green, blue = original.split()
+        wred, wgreen, wblue = watermark.split()
+    else:
+        red, green, blue, alpha = original.split()
+        wred, wgreen, wblue, walpha = watermark.split()
 
     red2 = ImageMath.eval("convert(a&0xFE|b&0x1, 'L')", a = red, b = wred)
     green2 = ImageMath.eval("convert(a&0xFE|b&0x1, 'L')", a = green, b = wgreen)
@@ -13,19 +28,17 @@ def encrypt(original, watermark, output):
     out.save(output)
 
 def decrypt(water, output): 
-    red, green, blue = water.split()
-    watermark = ImageMath.eval("(a&0x1)*255", a = blue)
-    #watermark = ImageMath.eval("(a&0x1)*255", a = green)
-    #watermark = ImageMath.eval("(a&0x1)*255", a = red)
-    watermark = watermark.convert("L")
-    watermark.save(output)
+    for colName, color in zip(colorNames, water.split()):
+        print color
+        watermark = ImageMath.eval("(a&0x1)*255", a = color)
+        watermark = watermark.convert("L")
+        watermark.save(colName + output)
 
 #main
 argLength = len(sys.argv)
 if argLength == 4:
-    watermark = Image.open(str(sys.argv[1]))
-    original = Image.open(str(sys.argv[2]))
-    watermark = watermark.resize(original.size)
+    watermark = str(sys.argv[1])
+    original = str(sys.argv[2])
     output = str(sys.argv[3])
     encrypt(original, watermark, output)
 else :
