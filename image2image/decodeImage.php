e6d2e6e8403a8ef7fea010664005956dda7b5c50
MisterDoom <raulignatus@yahoo.com>
2016-10-30 09:16:23 +0000

    Php integration of Py script

---
+++ image2image/decodeImage.php
@@ -0,0 +1,22 @@
+<?php
+echo "<a href='encodeImage.php'>Encode</a> - <a href='decodeImage.php'>Decode</a><br><br>";
+if ( $_POST['submit'] == "" ) {
+    echo "<form action='' method='post' enctype='multipart/form-data'>
+    <p><label>Carrier file </label><input type='file' name='file'></p>
+    <p><input type='submit' name='submit'></p>
+    </form>";
+} else {
+    move_uploaded_file($_FILES['file']['tmp_name'], "/home/pi/www/upload/upload.png");
+    $process = exec("/home/pi/www/python/./image2image.py -d /home/pi/www/upload/upload.png -o /home/pi/www/upload/out.png");
+    echo "Processing...<br><br>";
+    flush();
+    sleep(1);
+    echo "Completed ! <br>";
+    echo "<img src='../upload/out.png'>";
+    
+}
+
+
+
+
+?>
